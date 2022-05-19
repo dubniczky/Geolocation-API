3f29a48388adeda9a1d9d8aa65c835a91c0f6d84
Richard Antal Nagy <nagy.richard.antal@gmail.com>
2022-05-19 20:17:09 +0200

    Added search and random tests

--- tests/api.test.js
+++ tests/api.test.js
@@ -2,6 +2,7 @@ import request from 'supertest'
 
 import app from 'geocity/server.js'
 
+// locate
 describe('Endpoint: /locate tests', () =>
 {
     it('Accuracy: Given coordinates should return Budapest, Hungary', async () => {
@@ -19,4 +20,53 @@ describe('Endpoint: /locate tests', () =>
         expect(res2.status).toBe(200)
         expect(res1.body.ascii_name).toBe(res2.body.ascii_name)
     })
+})
+
+// search
+describe('Endpoint: /search tests', () =>
+{
+    it('Accuracy: The term budapest should return Budapest, Hungary', async () => {
+        const res = await request(app).get('/api/v1/search?q=budapest')
+        
+        expect(res.status).toBe(200)
+        expect(res.body.ascii_name).toBe('Budapest')
+    })
+
+    it('Stability: non-existing city should return 404 status', async () => {
+        const res = await request(app).get('/api/v1/search?q=doesnotexist')
+        
+        expect(res.status).toBe(404)
+    })
+
+    it('Validity: invalid query should return 400 status', async () => {
+        const res = await request(app).get('/api/v1/search')
+        
+        expect(res.status).toBe(400)
+    })
+})
+
+// random
+describe('Endpoint: /random tests', () =>
+{
+    it('Accuracy: a random query should return a valid country', async () => {
+        const res = await request(app).get('/api/v1/random')
+        
+        expect(res.status).toBe(200)
+        expect(res.body.ascii_name).toBeDefined()
+    })
+
+    it('Consistency: multiple queries should return different results', async () => {
+        let last = null
+        while (true) {
+            let res = await request(app).get('/api/v1/random')
+            expect(res.status).toBe(200)
+            if (last != res) {
+                break
+            }
+
+            console.log('Random match.')
+            last = res
+        }
+
+    }, 1000)
 })
