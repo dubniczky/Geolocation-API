258c71712bc9fd8874f84d4d6a337cecf24adcfc
Richard Antal Nagy <nagy.richard.antal@gmail.com>
2022-05-19 19:02:53 +0200

    Exported coordinate distance calculations to module

---
+++ modules/coord-distance.js
@@ -0,0 +1,13 @@
+function coordDistance(a_lat, a_lon, b_lat, b_lon) {
+    const lat = a_lat - b_lat
+    const lon = a_lon - b_lon
+    return Math.sqrt( lat*lat + lon*lon )
+}
+
+export function cityDistance(lat, lon, cityIndex, cities) {
+    return coordDistance(
+        lat,
+        lon,
+        cities[cityIndex]['coordinates'][0],
+        cities[cityIndex]['coordinates'][1])
+}
--- routers/api/v1/router.js
+++ routers/api/v1/router.js
@@ -1,24 +1,11 @@
 import { Router } from 'express'
 import { load, makeSearchList } from 'geocity/modules/data-loader.js'
+import { cityDistance } from 'geocity/modules/coord-distance.js'
 
 
 const cities = load('cities.json')
 const search = makeSearchList(cities)
 const router = Router()
-
-function coordDistance(a_lat, a_lon, b_lat, b_lon) {
-    const lat = a_lat - b_lat
-    const lon = a_lon - b_lon
-    return Math.sqrt( lat*lat + lon*lon )
-}
-
-function cityDistance(lat, lon, cityIndex) {
-    return coordDistance(
-        lat,
-        lon,
-        cities[cityIndex]['coordinates'][0],
-        cities[cityIndex]['coordinates'][1])
-}
 
 
 router.get('/locate', async (req, res) => {
@@ -30,17 +17,20 @@ router.get('/locate', async (req, res) => {
     }
 
     let closestIndex = 0
-    let dist = cityDistance(lat, lon, 0)
+    let dist = cityDistance(lat, lon, 0, cities)
 
     for (let i in cities) {
-        let d = cityDistance(lat, lon, i)
+        let d = cityDistance(lat, lon, i, cities)
         if (d < dist) {
             dist = d
             closestIndex = i
         }
     }
 
-    return res.send(cities[closestIndex])
+    let city = { ... cities[closestIndex] }
+    city.distance = dist * 111
+
+    return res.send(city)
 })
 
 router.get('/search', async (req, res) => {
