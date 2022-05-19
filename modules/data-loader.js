97bb3b9bd276ecbd9cfdd4968468fde3f3f4224a
Richard Antal Nagy <nagy.richard.antal@gmail.com>
2022-05-19 17:39:18 +0200

    Added perf metrics and fields export to data loader

--- modules/data-loader.js
+++ modules/data-loader.js
@@ -4,8 +4,21 @@ import path from 'path'
 
 
 function dataLoader(name) {
+    const start = performance.now()
     const fpath = path.join('data', name)
-    return load( readFileSync(fpath, 'utf8') )
+    console.log('Loading: ', fpath)    
+    const data = load( readFileSync(fpath, 'utf8') )
+
+    // Extract fields only
+    for (let i in data) {
+        data[i] = data[i].fields
+    }
+
+    // Print performance
+    const stop = performance.now()
+    console.log('Loaded ', data.length, ' items in:', (stop-start)/1000, 's')
+
+    return data
 }
 
 
