7055cc4adbfd9089170cbafcdb95d04ff69f3ffe
Richard Antal Nagy <nagy.richard.antal@gmail.com>
2022-05-19 19:45:56 +0200

    Added locate consistency test, changed test city

--- tests/api.test.js
+++ tests/api.test.js
@@ -4,10 +4,19 @@ import app from 'geocity/server.js'
 
 describe('Endpoint: /locate tests', () =>
 {
-    it('Given coordinates should return Vac, Hungary', async () => {
-        const res = await request(app).get('/api/v1/locate?lat=47.7832611&lon=19.143853')
+    it('Accuracy: Given coordinates should return Budapest, Hungary', async () => {
+        const res = await request(app).get('/api/v1/locate?lat=	47.49836&lon=19.04044')
         
         expect(res.status).toBe(200)
-        expect(res.body.ascii_name).toBe('Vac')
+        expect(res.body.ascii_name).toBe('Budapest')
+    })
+
+    it('Consistency: two searches of the same location should return the same city', async () => {
+        const res1 = await request(app).get('/api/v1/locate?lat=12.0&lon=-3.002')
+        const res2 = await request(app).get('/api/v1/locate?lat=12.0&lon=-3.002')
+        
+        expect(res1.status).toBe(200)
+        expect(res2.status).toBe(200)
+        expect(res1.body.ascii_name).toBe(res2.body.ascii_name)
     })
 })
