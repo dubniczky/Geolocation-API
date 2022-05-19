eb32c29fd7c0d8b1ac82a08e1a6f7c60f3c2b3e6
Richard Antal Nagy <nagy.richard.antal@gmail.com>
2022-05-19 18:49:39 +0200

    Added city distance function

--- routers/api/v1/router.js
+++ routers/api/v1/router.js
@@ -6,10 +6,18 @@ const cities = load('cities.json')
 const search = makeSearchList(cities)
 const router = Router()
 
-function geoDistance(a_lat, a_lon, b_lat, b_lon) {
+function coordDistance(a_lat, a_lon, b_lat, b_lon) {
     const lat = a_lat - b_lat
     const lon = a_lon - b_lon
     return Math.sqrt( lat*lat + lon*lon )
+}
+
+function cityDistance(lat, lon, cityIndex) {
+    return coordDistance(
+        lat,
+        lon,
+        cities[cityIndex]['coordinates'][0],
+        cities[cityIndex]['coordinates'][1])
 }
 
 
@@ -22,18 +30,10 @@ router.get('/locate', async (req, res) => {
     }
 
     let closestIndex = 0
-    let dist = geoDistance(
-        lat,
-        lon,
-        cities[0]['coordinates'][0],
-        cities[0]['coordinates'][1])
+    let dist = cityDistance(lat, lon, 0)
 
     for (let i in cities) {
-        let d = geoDistance(
-            lat,
-            lon,
-            cities[i]['coordinates'][0],
-            cities[i]['coordinates'][1])
+        let d = cityDistance(lat, lon, i)
         if (d < dist) {
             dist = d
             closestIndex = i
